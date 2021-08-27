fe43dd239116b06f3c3b863f5a8388ff06c2a47c
Jose E S Pimentel <40005153+jespimentel@users.noreply.github.com>
2021-08-27 15:26:29 -0300

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,44 @@
+################################################################
+#
+# Extrai áudio ou vídeo a partir de uma lista de URLs do YouTube
+#
+#                 Autor: José E. S. Pimentel
+#                       @jespimentel
+#                           2021
+#
+################################################################
+
+import pafy
+
+pasta_de_download = 'C:\\Users\\jepim\\Downloads' # Altere conforme o caso
+opcao = input ('Somente audio? (y/n): ')
+lista = input ("Entre com a lista de URLs separadas por ',': ")
+lista = lista.split(',')
+print()
+
+def get_audio(url_video, pasta_de_download):
+  try:
+    v = pafy.new(url_video)
+    audio = v.getbestaudio(preftype='m4a')
+    print ('Baixando o áudio de ' + url_video)
+    audio.download(pasta_de_download)
+  except:
+    print ('Erro com a url: ' + url_video + '\n')
+
+def get_video(url_video, pasta_de_download):
+  try:
+    v = pafy.new(url_video)
+    video = v.getbest()
+    print ('Baixando o vídeo de ' + url_video)
+    video.download(pasta_de_download)
+  except:
+    print ('Erro com a url: ' + url_video + '\n')
+
+for l in lista:
+    l = l.replace (' ', '')
+    if opcao == 'y':
+        get_audio(l, pasta_de_download)
+    else:
+        get_video(l, pasta_de_download)
+
+print ('Programa concluído')
